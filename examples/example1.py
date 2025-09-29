61b0c933d350ef9eaa4f1e1f738b6dcbeb4c2ee3
Banana-programadora <damofeasta@gmail.com>
2025-09-29 22:42:19 +0200

    Update example1.py

--- examples/example1.py
+++ examples/example1.py
@@ -2,11 +2,11 @@ import minibanana as mb
 
 mb.window("Test", 500, 500, "white")
 
-jugador = mb.draw_sprite(100, 100, "basic_human.png")
-laser = mb.draw_rect(200, 100, 25, 25, "red")
-texto = mb.draw_text(300, 300, "TUTORIAL", 30, "black")
+jugador = mb.draw_sprite(100, 100, "basic_human.png") #POSICION X, POSICION Y, ARCHIVO DE LA IMAGEN
+laser = mb.draw_rect(200, 100, 25, 25, "red") #POSICION X, POSICION Y, TAMAÑO X, TAMAÑO Y, COLOR
+texto = mb.draw_text(300, 300, "TUTORIAL", 30, "black"), POSICION X, POSICION Y, TEXTO, TAMAÑO DE LETRA, COLOR, FUENTE
 
-mb.get_keys()
+mb.get_keys() #DETECTA LEFT, RIGHT, UP Y DOWN
 
 def logica():
     if mb.is_key_pressed("Left"):
@@ -21,4 +21,4 @@ def logica():
         quit()
 
 mb.set_logic(logica)
-mb.start(10)
+mb.start(10) # 10 REPRESENTA LOS MS POR FRAME, UN 16 SON UNOS 60 FPS
