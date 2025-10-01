ce9e5ede36a0e6526b8df7204810a5f7ea5c71c9
Banana-programadora <damofeasta@gmail.com>
2025-10-01 21:35:05 +0200

    Arreglo importaciones relativas en __init__.py

--- minibanana/__init__.py
+++ minibanana/__init__.py
@@ -10,3 +10,16 @@ from core import (
     start,
     set_logic
 )
+
+__all__ = [
+    "window",
+    "draw_rect",
+    "draw_sprite",
+    "draw_text",
+    "get_keys",
+    "colliderect",
+    "play_sound",
+    "stop_sound",
+    "start",
+    "set_logic",
+]
