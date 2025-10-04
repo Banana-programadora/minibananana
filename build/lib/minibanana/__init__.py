54d1c5642725d299ae095e903d5566857f320790
Banana-programadora <damofeasta@gmail.com>
2025-10-04 10:19:11 +0200

    Soluciones de bugs

--- build/lib/minibanana/__init__.py
+++ build/lib/minibanana/__init__.py
@@ -1,4 +1,4 @@
-from core import (
+from .core import (
     window,
     draw_rect,
     draw_sprite,
@@ -10,3 +10,16 @@ from core import (
     start,
     set_logic
 )
+
+__all__ = [
+    "window",
+    "draw_rect",
+    "draw_sprite",
+    "draw_text",
+    "get_keys",
+    "colliderect",
+    "play_sound",
+    "stop_sound",
+    "start",
+    "set_logic",
+]
